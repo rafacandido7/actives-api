ef3d4e670c8b69560ea35644f535bf34eba4f6ed
Rafael Candido <rafacandido0708@gmail.com>
2023-11-28 22:49:45 -0300

    fix: update actives/dependencies

--- src/modules/actives/dto/update-active.dto.ts
+++ src/modules/actives/dto/update-active.dto.ts
@@ -1,13 +1,3 @@
-import { IsUUID } from 'class-validator'
-import { ApiProperty } from '@nestjs/swagger'
-
 import { CreateActiveDto } from './create-active.dto'
 
-export class UpdateActiveDto extends CreateActiveDto {
-  @ApiProperty({
-    description: 'Id of the active',
-    example: 'c467aa4d-470e-4da0-b616-65382a11cb1a',
-  })
-  @IsUUID()
-  id: string
-}
+export class UpdateActiveDto extends CreateActiveDto {}
